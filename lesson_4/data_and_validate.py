6b96d35beb0a0774ab42886b4be56459c7003cd2
sem1on <sgorlov21@gmail.com>
2025-04-15 21:47:23 +0300

    add assertion for url and title

--- lesson_4/data_and_validate.py
+++ lesson_4/data_and_validate.py
@@ -7,10 +7,14 @@ service = Service(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
 
 url = "https://www.wikipedia.org/"
-
 driver.get(url=url) 
 
 current_url = driver.current_url
-current_title = driver.title
 print(f"URL запрашиваемой страницы: {current_url}.")
+assert current_url == url, "Открыта не та страница, ошибка в URL"
+
+current_title = driver.title
 print(f"Title запрашиваемой страницы: {current_title}.")
+assert current_title == "Wikipedia", "Title страницы не совпадает"
+
+print(driver.page_source)
