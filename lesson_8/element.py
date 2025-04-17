efa3c991ee7ad09c72a67bdaecc4ef68b2cdaebe
sem1on <sgorlov21@gmail.com>
2025-04-17 22:35:24 +0300

    add options

--- lesson_8/element.py
+++ lesson_8/element.py
@@ -3,7 +3,6 @@ import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
 
 
 service = Service(executable_path=ChromeDriverManager().install())
---
+++ lesson_9/options.py
@@ -0,0 +1,16 @@
+import time
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--headless") # завпуск в фоне
+chrome_options.add_argument("--incognito") # режим ингогнито
+chrome_options.add_argument("--ignore-certificate-errors") # пропуск отсутствия сертификата
+chrome_options.add_argument( "--disable-cache") # отключить кэш
+chrome_options.add_argument("--window-size=X,Y") # размер окна
+
+service = Service(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=chrome_options)
