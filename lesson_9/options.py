a986cf64434d0fceb5e7e74bce67a32ded1cee23
sem1on <sgorlov21@gmail.com>
2025-04-17 22:39:44 +0300

    add options

--- lesson_9/options.py
+++ lesson_9/options.py
@@ -6,6 +6,10 @@ from selenium.webdriver.chrome.service import Service
 
 
 chrome_options = webdriver.ChromeOptions()
+
+chrome_options.page_load_strategy = ("normal") # ожидание полной загрузки страницы (дефолт)
+chrome_options.page_load_strategy = ("eager") # ожидание толькло DOM
+
 chrome_options.add_argument("--headless") # завпуск в фоне
 chrome_options.add_argument("--incognito") # режим ингогнито
 chrome_options.add_argument("--ignore-certificate-errors") # пропуск отсутствия сертификата
