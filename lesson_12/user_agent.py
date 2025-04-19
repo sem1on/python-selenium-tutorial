87f984e6dc8d511ff24df4ce6c4a781d0a52219e
sem1on <sgorlov21@gmail.com>
2025-04-19 20:55:52 +0300

    add user-agenr option

--- lesson_12/user_agent.py
+++ lesson_12/user_agent.py
@@ -11,6 +11,7 @@ from selenium.webdriver.support import expected_conditions as EC
 options = Options()
 options.add_argument("--window-size=1920,1080")
 options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_argument("--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36")
 
 service = Service(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=options)
