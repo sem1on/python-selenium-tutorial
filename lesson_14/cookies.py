e24872f34469ff49ca632e02d0fa7ab87ae05f2b
sem1on <sgorlov21@gmail.com>
2025-04-20 17:21:27 +0300

    add swap cookies

--- lesson_14/cookies.py
+++ lesson_14/cookies.py
@@ -3,12 +3,15 @@ import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
+options = Options()
+options.add_argument("--disable-blink-features=AutomationControlled")
 service = Service(executable_path=ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service)
+driver = webdriver.Chrome(service=service, options=options)
 wait = WebDriverWait(driver, 10, poll_frequency=1)
 
 driver.get("https://www.freeconferencecall.com/ru")
@@ -20,6 +23,12 @@ add_cookie = driver.add_cookie({ # Добавление своих cookies
     "name": "My_cookie",
     "value": "21"
 })
-
 my_cookie = driver.get_cookie("My_cookie")
-print(my_cookie)
+
+driver.delete_cookie("split") # Замена cookie через удаление и добавление
+after = driver.add_cookie({
+    "name": "split",
+    "value": "new_value"
+})
+
+print(driver.get_cookie("split"))
