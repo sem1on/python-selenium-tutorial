25dba0bb9e12abbce7d05ac43f2ce9768511fd2c
sem1on <sgorlov21@gmail.com>
2025-04-21 20:38:21 +0300

    add select

---
+++ lesson_16/3_multiselect.py
@@ -0,0 +1,18 @@
+import time
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver import Keys
+
+
+service = Service(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+base_url = "https://demoqa.com/select-menu"
+
+SELECT_LOCATOR = ("xpath", "input[@id='react-select-3-input']")
+
+driver.get(base_url)
+
+driver.find_element(*SELECT_LOCATOR).send_keys("Ms.")
+driver.find_element(*SELECT_LOCATOR).send_keys(Keys.ENTER)
